c2503548aaeda5fb25742c8a8d519d8fbd0b245f
One2mellow <Nitai1998>
2022-05-18 21:09:59 +0300

    fixed deleted code

--- linear_solutions/Source.c
+++ linear_solutions/Source.c
@@ -1,7 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
+	int num,i,j;
+	float** mtrx, *x,*b;
+	char** equ;
+
+	printf_s("Number of equations (1-3): ");
+	scanf_s("%d", &num);
+	getchar();
+	// Create dynamic array
+	mtrx = malloc(sizeof(float*) * num);
+	for (i = 0;i < num;i++)
+		mtrx[i] = malloc(sizeof(float) * num);
+	
+	equ = malloc(sizeof(char*) * num);
+	if (equ)
+	{
+		for (i = 0;i < num;i++)
+			equ[i] = malloc(sizeof(char) * 81);
+	}
+	
+	x = malloc(sizeof(float) * num);
+	b = malloc(sizeof(float) * num);
+	
+
+	if (equ)
+	{
+		for (i = 0; i < num; i++) {
+			printf_s("Enter equation %d: ", i + 1);
+			gets_s(equ[i], 80);
+		}
+	}
+	//// init array
+	//for (i = 0;i < num;i++)
+	//	for (j = 0;j < num;j++)
+	//		mtrx[i][j] = 0;
+	//puts("");
+	//for (i = 0;i < num;i++) {
+	//	for (j = 0;j < num;j++) {
+	//		mtrx[i][j] = i + j;
+	//		printf_s("%.2f ", mtrx[i][j]);
+	//	}
+	//	printf_s("\n");
+	//}
+
+
+
+	for (i = 0;i < num;i++)
+		free(mtrx[i]);
+	free(mtrx);
+
+	for (i = 0;i < num;i++)
+		free(equ[i]);
+	free(equ);
+
+	free(x);
+	free(b);
 
 
 	return 0;
