346a30fdbc7c0fdae95ade797f3ef56688c98738
One2mellow <Nitai1998>
2022-05-21 14:30:16 +0300

    Added feature cutting equation coefficient correctly

--- linear_solutions/Source.c
+++ linear_solutions/Source.c
@@ -7,6 +7,8 @@ float parsing(char* equation, char coef, int length);
 
 char order(char* equation, int length);
 
+void cut(char* equation, int length, char coef);
+
 int main() {
 	int num, i, j;
 	float** mtrx, * x, * b;
@@ -16,6 +18,8 @@ int main() {
 	printf_s("Number of equations (1-3): ");
 	scanf_s("%d", &num);
 	getchar();
+
+
 	// Create dynamic array
 	mtrx = malloc(sizeof(float*) * num);
 	for (i = 0;i < num;i++)
@@ -38,25 +42,43 @@ int main() {
 			printf_s("Enter equation %d: ", i + 1);
 			gets_s(equ[i], 80);
 		}
+
 	}
 
-	coef = order(equ[0], strlen(equ[0]));
+	if (mtrx && equ)
+	{
+		for (i = 0;i < num; i++) {
+			for (j = 0; j < num; j++)
+			{
+				coef = order(equ[i], strlen(equ[i]));
+				mtrx[i][j] = parsing(equ[i], coef, strlen(equ[i]));
+				cut(equ[i], strlen(equ[i]), coef);
+			}
+		}
+	}
 
-	mtrx[0][0] = parsing(equ[0], coef, strlen(equ[0]));
 
+	for (i = 0; i < num; i++)
+	{
+		//printf("%s\n", equ[i]);
+		for ( j = 0; j < num; j++)
+		{
+			printf("%.2f\n", mtrx[i][j]);
+		}
+	}
 
 	//// init array
 	//for (i = 0;i < num;i++)
 	//	for (j = 0;j < num;j++)
 	//		mtrx[i][j] = 0;
 	//puts("");
-	//for (i = 0;i < num;i++) {
-	//	for (j = 0;j < num;j++) {
-	//		mtrx[i][j] = i + j;
-	//		printf_s("%.2f ", mtrx[i][j]);
-	//	}
-	//	printf_s("\n");
-	//}	
+	for (i = 0;i < num;i++) {
+		for (j = 0;j < num;j++) {
+//			mtrx[i][j] = i + j;
+			printf_s("%.2f ", mtrx[i][j]);
+		}
+		printf_s("\n");
+	}	
 
 
 
@@ -107,25 +129,22 @@ float parsing(char* equation, char coef, int length) {
 		}
 	}
 
-	puts(temp);
-
 	co = atof(temp);
 
 
-	free(temp);
+	//free(temp);
 	return co;
 }
 
 
 char order(char* equation, int length) {
 	int i;
-	char coef;
+	char coef ='x';
 	char* temp;
 
 	temp = malloc(sizeof(char*) * length);
 
-	strcpy(temp, equation);
-
+	strcpy_s(temp ,sizeof(char*)*length, equation);
 	for (size_t i = 0; i < length; i++)
 	{
 		if (temp[i] == 'x' || temp[i] == 'y' || temp[i] == 'z') {
@@ -136,4 +155,30 @@ char order(char* equation, int length) {
 
 	free(temp);
 	return coef;
+}
+
+
+
+void cut(char* equation, int length, char coef) {
+	int j,i = 0;
+	char* dex;
+
+	for (i = 0; i < length; i++)
+	{
+		if (equation[i] == coef);
+		{
+			printf("\n***%c***\n", equation[i+1]);
+			for (j = 0; equation[i]!='\0' ; j++) {
+				equation[j] = equation[i+2];
+				i++;
+			}
+			equation[i + 1] = '\0';
+		}
+	}
+
+
+	//puts(equation[dex-equation]);
+
+	//equation[i + 1] = '\0';
+
 }
